b057cdda06e96e9904995a3d4ba0da15e833a7c4
rivaldz <rivaldz@Rivaldz>
2018-11-26 20:42:42 +0700

    memodifikasi item penjualan supaya dapat memasukan data atribut ke gui

--- src/rivaldo/ItemPenjualan.java
+++ src/rivaldo/ItemPenjualan.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rivaldo;
+
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author rivaldz
  */
 public class ItemPenjualan {
+	private String nama;
+	private int jumlah ;
+	private float harga;
+	private DefaultTableModel tabel = new DefaultTableModel();
 	
+	public ItemPenjualan() {
+	getTabel().addColumn("Barang");
+        getTabel().addColumn("Harga");
+        getTabel().addColumn("Qty");
+        getTabel().addColumn("Satuan");
+        getTabel().addColumn("Jumlah");
+	
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public int getJumlah() {
+		return jumlah;
+	}
+
+	public void setJumlah(int jumlah) {
+		this.jumlah = jumlah;
+	}
+
+	public float getHarga() {
+		return harga;
+	}
+
+	public void setHarga(float harga) {
+		this.harga = harga;
+	}
+
+	public DefaultTableModel getTabel() {
+		return tabel;
+	}
+
+	public void setTabel(DefaultTableModel tabel) {
+		this.tabel = tabel;
+	}
+	
+	
+
 }
+	
+
