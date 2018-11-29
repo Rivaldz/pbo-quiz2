e682d621830f39b6d96589a8297c736bde5c6b35
rivaldz <rivaldz@Rivaldz>
2018-11-29 19:31:01 +0700

    menampilan hasil transaksi apabila membeli lebih dari 1 jenis barang

--- src/rivaldo/ItemInputTable.java
+++ src/rivaldo/ItemInputTable.java
@@ -9,15 +9,15 @@ package rivaldo;
  *
  * @author rivaldz
  */
-public class ItemInputTable {
-
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		// TODO code application logic here
-		Transaksi transaksi = new Transaksi();
-		transaksi.setVisible(true);
-	}
-	
-}
+//public class ItemInputTable {
+//
+//	/**
+//	 * @param args the command line arguments
+//	 */
+//	public static void main(String[] args) {
+//		// TODO code application logic here
+//		Transaksi transaksi = new Transaksi(); 
+//		transaksi.setVisible(true);
+//	}
+//	
+//}
